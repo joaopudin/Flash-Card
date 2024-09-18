7546b3660f85d052cdd7c5bedee84d3ad696b919
joaopudin <140727717+joaopudin@users.noreply.github.com>
2024-09-18 19:52:09 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,175 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Marvel',
+    'Quando foi lançado o primeiro filme do Homem de Ferro?',
+    'O filme foi lançado em 2008'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Marvel',
+    'Quanto foi a bilheteria do filme "Vingadores"?',
+    'A bilheteria foi de 1,5 bilhões de dólares no mundo todo'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Marvel',
+    'Qual o nome verdadeiro do Deadpool?',
+    'O nome dele é Wade Wilson'
+)
+
+criaCartao(
+    'Marvel',
+    'Quais são as seis jóias do Infinito?',
+    'As jóias são: Realidade, Mente, Alma, Poder, Tempo e Espaço'
+)
+
+criaCartao(
+    'Marvel',
+    'Por quanto tempo Capitão América ficou congelado?',
+    'Ficou congelado por 67 anos, de 1945 até 2012'
+)
+
+criaCartao(
+    'Marvel',
+    'Qual a idade do Thor?',
+    'O Deus do Tovão na Marvel tem 1.493 anos de idade'
+)
+
+criaCartao(
+    'Dc',
+    'Quem interpreta o Coringa no segundo filme da franquia: Batman Cavaleiro das Trevas?',
+    'O nome do ator é Heath Ledger'
+)
+
+criaCartao(
+    'Dc',
+    'Quem é o diretor dos três filmes da trilogia Batman?',
+    'O diretor é Christopher Nolan'
+)
+
+criaCartao(
+    'Dc',
+    'Como Harvey Dent virou o Duas-Caras?',
+    'Ele vira Duas-Caras após Sal Maroni jogar ácido nele durante um julgamento, desfigurando o lado esquerdo do seu rosto'
+)
+
+criaCartao(
+    'Dc',
+    'Como o Cringa ficou louco?',
+    'Coringa era um humorista mal-sucedido que tinha uma esposa grávida, então começou a se envolver com criminosos para assaltar uma fábrica de baralho usando a identidade de Capuz Vermelho, até que numa invasão mal-sucedida, caiu num tonel de produtos ácidos, ganhou a aparência mais famosa.'
+)
+
+criaCartao(
+    'Dc',
+    'Qual o nome verdadeiro do Superman?',
+    'O nome dele é Kal-El'
+)
+
+criaCartao(
+    'Dc',
+    'Quem são os membros originais da Liga da Justiça',
+    'Os membros são: Superman, Batman, Aquaman, Mulher Maravilha, Lanterna Verde, Flash, Caçador de Marte e Ciborgue'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
